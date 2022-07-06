5275bb1cc13c1137797b99f337945d459b4dd096
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-06 16:54:25 -0400

    updated day 2

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var blogsRouter = require("./routes/blogs");
 
 var app = express();
-var blogsRouter = require("./routes/blogs");
-app.use("/blogs", blogsRouter);
+var { mongoConnect } = require("./mongo.js");
+mongoConnect();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+app.options("*", cors());
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -23,13 +32,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/blogs", blogsRouter);
 
-// catch 404 and forward to error handler
+// catch 404 plus forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler page
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,18 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/hello-blogs", function (req, res, next) {
+const express = require("express");
+const router = express.Router();
+const { blogsDB } = require("../mongo");
+router.get("/hello-blogs", (req, res, next) => {
   res.json({ message: "hello from express" });
+});
+
+router.get("/all-blogs", async (req, res) => {
+  try {
+    const collection = await blogsDB().collection("blogs50");
+    const posts = await collection.find({}).toArray();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send("error fetching posts " + error);
+  }
 });
 
 module.exports = router;
