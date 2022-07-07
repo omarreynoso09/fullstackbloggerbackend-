4c4db1420059eb302368ff4813334a33a84bee13
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-07 11:28:01 -0400

    last commit

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const { blogsDB } = require("../mongo");
-router.get("/hello-blogs", (req, res, next) => {
+
+router.get("/hello-blogs", (req, res) => {
   res.json({ message: "hello from express" });
 });
 
@@ -9,9 +11,9 @@ router.get("/all-blogs", async (req, res) => {
   try {
     const collection = await blogsDB().collection("blogs50");
     const posts = await collection.find({}).toArray();
-    res.json(posts);
-  } catch (error) {
-    res.status(500).send("error fetching posts " + error);
+    res.send({ message: posts });
+  } catch (e) {
+    res.status(500).send("Error fetching posts." + e);
   }
 });
 
