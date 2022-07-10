cd7bea2992c586059953f6cf1a4481cf88370fc7
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-10 19:42:10 -0400

    day 3

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.options("*", cors());
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,12 +32,12 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/blogs", blogsRouter);
 
-// catch 404 plus forward to error handler
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler page
+// error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
+
+// const { Db } = require('mongodb');
 const { blogsDB } = require("../mongo");
 
 router.get("/hello-blogs", (req, res) => {
   res.json({ message: "hello from express" });
 });
 
-// if stamements --- sort field is equal to "ASC", then sequential (1) and the opposite (-1).
-router.get("/all-blogs", async (req, res) => {
-  const limit = Number(req.query.limit);
-  const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
-  const sortField = req.query.sortField;
-  const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
-  const filterField = req.query.filterField;
-  const filterValue = req.query.filterValue;
-
+router.get("/all-blogs", async function (req, res, next) {
   try {
+    console.log(req.query.filterValue);
     const collection = await blogsDB().collection("blogs50");
+    const limit = Number(req.query.limit);
+    const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
+    const sortField = req.query.sortField;
+    const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
+    const filterField = req.query.filterField;
+    const filterValue = req.query.filterValue;
     let filterObj = {};
     if (filterField && filterValue) {
       filterObj = { [filterField]: filterValue };
@@ -25,18 +26,51 @@ router.get("/all-blogs", async (req, res) => {
     if (sortField && sortOrder) {
       sortObj = { [sortField]: sortOrder };
     }
-
-    // const posts = await collection.find({}).toArray();
-    const posts = await collection
+    const blogs50 = await collection
       .find(filterObj)
       .sort(sortObj)
       .limit(limit)
       .skip(skip)
       .toArray();
+    res.json({ message: blogs50 });
+  } catch (e) {
+    res.status(500).send("Error fetching posts.");
+  }
+});
+router.post("/blog-submit", async function (req, res, next) {
+  try {
+    console.log("testing before blogPost creation");
+    const collection = await blogsDB().collection("blogs50");
 
-    res.json({ message: posts });
-  } catch (error) {
-    res.status(500).send("error fetching posts " + error);
+    const sortedBlogArray = await collection.find({}).sort({ id: 1 }).toArray();
+
+    const lastBlog = sortedBlogArray[sortedBlogArray.length - 1];
+    console.log("req body", req.body);
+
+    const title = req.body.title;
+    console.log("title", title);
+
+    const text = req.body.text;
+    console.log("text", req.body.text);
+
+    const author = req.body.author;
+    const category = req.body.category;
+
+    const blogPost = {
+      title: title,
+      text: text,
+      author: author,
+      category: category,
+      createdAt: new Date(),
+      id: Number(lastBlog.id + 1),
+      lastModified: new Date(),
+    };
+    console.log(blogPost);
+
+    await collection.insertOne(blogPost);
+    // res.status(200).send("Post submitted");
+  } catch (e) {
+    res.status(500).send("Error fetching posts." + e);
   }
 });
 
